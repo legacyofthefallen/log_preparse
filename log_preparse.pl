02425668bf461caa8edc36d915a3b1a61f7d2d44
Dan Stephans <adept@stephans.org>
2001-07-08 20:57:38 -0800

    squash to root of master
    Create README.md
    
    Update README.md
    
    Update README.md
    
    added .gitignore
    
    Initial Commit
    
    Link to site hosting player history
    
    Link includes players submitted up until the Zek server merge
    
    added conditional that if file is not readable in script that output is given with name and next file is to be processed
    
    output will close on last iteration of loop
    log will close after log line read
    
    inclusion of object DateTime for parsing
    
    added conditional that if file is not readable in script that output is given with name and next file is to be processed
    
    timestemp regex changed for more specific capture of eq log format

---
+++ log_preparse.pl
@@ -0,0 +1,60 @@
+#!/bin/perl
+#
+# written by Dan Stephans II
+# (Rafinne L`Ongbone, Rallos Zek)
+use DateTime;
+
+@files = @ARGV;
+
+print "Output will be saved in file output.txt in current directory.\n";
+  if(-f $file && -w $file)
+    open( OUTPUT, ">output.txt" ) || die "Cannot open output.txt for write";
+  
+foreach $file ( @files )
+{
+
+  if( ! -f $file )
+  {
+    print "$file does not appear to be a file, skipping...\n";
+    next;
+  }
+  if (! -r $file)
+  {
+    print "$file is not readable, skipping...\n";
+    next;
+  }
+  if( $file eq "output.txt" )
+  {
+    print "output.txt skipped (you must have globbed)\n";
+    next;
+  }
+  open( LOG, "<$file" ); #|| die "Cannot open $file for read, aborting.";
+
+  while( <LOG> )
+  {
+    my $dt;
+    if( /\[(\w\w\w\s\w\w\w\s\d\d\s\d\d:\d\d:\d\d\s\d\d\d\d)\] (.*)/ )
+    {
+      # we break this out for our output so we can write a sortable file
+      # since the standard datestamp is wonky and we create a nice stamp
+      # that can be dropped right into mySQL
+      dt = DateTime::Format::HTTP->parse_datetime($1);
+      $_         = $2;
+    }
+    else
+    {
+      print "invalid log stamp so skip it";
+      next;
+    }
+    # test the remainder for valid strings
+    if( ! /^AFK/ && !/^\* / && !/^\[/ && !/is a member of/ && !/is the leader of/ && !/is an officer of/ && !/is not in a guild/)
+    {
+      next;
+    }
+    print OUTPUT "[$dt->year-$dt->month_abbr-$dt->day $dt->hms] ",$_, "\n";
+    last close(LOG);
+  }
+  last close( OUTPUT );;
+}
+
+print "All done.\n";
